5b1530921c66695c56a1a94b28b51729321eabdc
sauravBarua <myallmailofficial@gmail.com>
2021-04-07 16:33:49 +0200

    change all router userinfo to auth

---
+++ backend/routes/auth.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const router = express.Router();
+const UserModel = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const passwordValidator = require('password-validator');
+
+
+
+//Signup router
+router.post('/signup',
+    body('email').isEmail(),
+    body('password').custom((value) => {
+        var schema = new passwordValidator();
+        schema
+            .is().min(8)
+            .is().max(100)
+            .has().uppercase()
+            .has().lowercase()
+            .has().digits(2)
+            .has().not().spaces()
+            .is().not().oneOf(["Passw0rd", "Password123"]);
+        return schema.validate(value);
+    }),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const user = new UserModel({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            age: req.body.age,
+            platform: req.body.platform,
+            profilePicture: req.body.profilePicture
+        })
+        try {
+            const saveUser = await user.save()
+            res.json(saveUser)
+
+        } catch (err) {
+            res.json({ message: err })
+        }
+    })
+
+// router.post('/signup', async (req, res) => {
+//     const user = new UserModel({
+//         username: req.body.username,
+//         email: req.body.email,
+//         password: req.body.password,
+//         age: req.body.age,
+//         platform: req.body.platform,
+//         profilePicture: req.body.profilePicture
+//     })
+//     try {
+//         const saveUser = await user.save()
+//         res.json(saveUser)
+
+//     } catch (err) {
+//         res.json({ message: err })
+//     }
+
+// })
+
+router.post('/login', async (req, res) => {
+    const user = new UserModel({
+    username: req.body.username,
+    password: req.body.password,
+})
+try {
+    const findUser = await user.findOne()
+    res.json(findUser)
+
+} catch (err) {
+    res.json({ message: err })
+}
+
+})
+
+//Profil router
+router.get('/profile/:id', async (req, res) => {
+    try {
+        const userProfil = await UserModel.findById(req.params.id)
+        res.json(userProfil)
+
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+module.exports = router;
--- backend/routes/userInfo.js
+++ backend/routes/userInfo.js
@@ -1,47 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const UserModel = require('../models/User');
-const { body, validationResult } = require('express-validator');
-const passwordValidator = require('password-validator');
 
 
 
-//Signup router
-router.post('/signup',
-    body('email').isEmail(),
-    body('password').custom((value) => {
-        var schema = new passwordValidator();
-        schema
-            .is().min(8)
-            .is().max(100)
-            .has().uppercase()
-            .has().lowercase()
-            .has().digits(2)
-            .has().not().spaces()
-            .is().not().oneOf(["Passw0rd", "Password123"]);
-        return schema.validate(value);
-    }),
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        const user = new UserModel({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            age: req.body.age,
-            platform: req.body.platform,
-            profilePicture: req.body.profilePicture
-        })
-        try {
-            const saveUser = await user.save()
-            res.json(saveUser)
 
-        } catch (err) {
-            res.json({ message: err })
-        }
-    })
+
 
 // router.post('/signup', async (req, res) => {
 //     const user = new UserModel({
