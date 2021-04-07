eeb1107ed75c85502276db3bc3f336957719daf7
SNarboni <sebastien.narboni@hotmail.fr>
2021-04-07 16:12:08 +0200

    test request

---
+++ backend/fixture.js
@@ -0,0 +1,11 @@
+const request = require("request");
+
+request(
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=efd8a07427b2c721a89376dbc34799dd",
+  function (error, response, body) {
+    console.error("error:", error); // Print the error if one occurred
+    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+   //console.log("body:", body); // Print the HTML for the Google homepage.
+   console.log(response.body)
+  }
+);
