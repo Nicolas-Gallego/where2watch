a59e3fab7e0bd8e80682400e1b97c1574d27b305
sauravBarua <myallmailofficial@gmail.com>
2021-04-08 11:08:30 +0200

    Working on login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -31,7 +31,7 @@ router.post('/signup',
             email: req.body.email,
             password: req.body.password,
             age: req.body.age,
-            platform: req.body.platform,
+           // platform: req.body.platform,
             profilePicture: req.body.profilePicture
         })
         try {
@@ -43,39 +43,32 @@ router.post('/signup',
         }
     })
 
-// router.post('/signup', async (req, res) => {
-//     const user = new UserModel({
-//         username: req.body.username,
-//         email: req.body.email,
-//         password: req.body.password,
-//         age: req.body.age,
-//         platform: req.body.platform,
-//         profilePicture: req.body.profilePicture
-//     })
-//     try {
-//         const saveUser = await user.save()
-//         res.json(saveUser)
-
-//     } catch (err) {
-//         res.json({ message: err })
-//     }
-
-// })
+//Login router
 
 router.post('/login', async (req, res) => {
-    const user = new UserModel({
-    username: req.body.username,
-    password: req.body.password,
-})
-try {
-    const findUser = await user.findOne()
-    res.json(findUser)
+    try {
+        const body = req.body
+        const user = await UserModel.findOne({
+            username: body.username
+        })
+        if (!user) {
+            return res.status(404).send("There are no user")
+        }
+        if (user.password !== body.password) {
+            return res.status(404).json({
+                isConnected: false
+            })
+        }
 
-} catch (err) {
-    res.json({ message: err })
-}
-
+    } catch (err) {
+        console.error(err)
+        res.status(404).json({
+            isConnected: false
+        })
+    }
 })
+
+
 
 //Profil router
 router.get('/profile/:id', async (req, res) => {
