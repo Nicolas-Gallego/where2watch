398dc9c3d2cc105222c0968e8bbc4b7b50030bf3
Nicolas Gallego <gallego.nicolaspro@gmail.com>
2021-04-18 03:29:40 +0200

    footer registred done

--- frontend/src/components/FooterRegistred.jsx
+++ frontend/src/components/FooterRegistred.jsx
@@ -29,30 +29,78 @@ const FooterRegistred = () => {
   };
 
   return (
-    <div className="container-fluid footer">
-      <div className="row">
-        <div className="centerMenu d-flex justify-content-center">
-          <div className="col-8 d-flex justify-content-between">
-            <div className="col-10 d-flex footerBrand">
-              <a href="/home" className="footerLogo">
+    // <div className="container-fluid footer">
+    //    <div className="row">
+    //     <div className="centerMenu d-flex justify-content-center">
+    //       <div className="col-8 d-flex justify-content-between">
+    //         <div className="col-10 d-flex footerBrand">
+    //           <a href="/home" className="footerLogo">
+    //             <img
+    //               src="/w2w_logo_white.png"
+    //               alt="w2w logo"
+    //               className="footerLogo"
+    //             />
+    //             <h3>Where 2 Watch</h3>
+    //           </a>
+    //         </div>
+    //         <div className="col-5">
+    //           <a href="/home" className="footerLink">
+    //             <h6>Home&thinsp;</h6>
+    //           </a>
+    //           <a href="/catalog" className="footerLink">
+    //             <h6>Search&thinsp;</h6>
+    //           </a>
+    //           <a href="/home" className="footerLink" onClick={() => {
+    //             localStorage.clear();
+    //           }}>
+    //             <h6>Logout&thinsp;</h6>
+    //           </a>
+    //           <a
+    //             className="footerLink"
+    //             href={`/profile/${myInfos._id}`}
+    //             onClick={() => checkToken()}
+    //           >
+    //             <h6>Profile</h6>
+
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className="d-flex justify-content-sm-end justify-content-center ">
+    //     <h6>Where 2 Watch©</h6>
+    //   </div>
+    // </div>
+    <>
+      <div className="container-fluid footer">
+        <div className="row">
+          <div className="col-12 d-flex justify-content-evenly ">
+            <div className="d-flex flex-column align-items-center">
+              <a href="/home" className="">
                 <img
                   src="/w2w_logo_white.png"
                   alt="w2w logo"
                   className="footerLogo"
                 />
+              </a>
+              <a href="/home" className="footerLogo">
                 <h3>Where 2 Watch</h3>
               </a>
             </div>
-            <div className="col-5">
+            <div className="">
               <a href="/home" className="footerLink">
                 <h6>Home&thinsp;</h6>
               </a>
               <a href="/catalog" className="footerLink">
                 <h6>Search&thinsp;</h6>
               </a>
-              <a href="/home" className="footerLink" onClick={() => {
-                localStorage.clear();
-              }}>
+              <a
+                href="/home"
+                className="footerLink"
+                onClick={() => {
+                  localStorage.clear();
+                }}
+              >
                 <h6>Logout&thinsp;</h6>
               </a>
               <a
@@ -61,16 +109,15 @@ const FooterRegistred = () => {
                 onClick={() => checkToken()}
               >
                 <h6>Profile</h6>
-                
               </a>
             </div>
           </div>
         </div>
       </div>
-      <div className="d-flex justify-content-sm-end justify-content-center ">
+      <div className="copiright">
         <h6>Where 2 Watch©</h6>
       </div>
-    </div>
+    </>
   );
 };
 export default FooterRegistred;
